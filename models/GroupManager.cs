ca324be8ba8695021143d07eb7ad19d9c5c0e3a2
0sekto0 <tomskiy_a@list.ru>
2019-03-29 23:01:26 +0300

    bot almost alive again

--- models/GroupManager.cs
+++ models/GroupManager.cs
@@ -43,7 +43,8 @@ namespace groupbot.Models
         //информация об отложенных постах
         private int PostponedInf()
         {
-            VkResponse response = _vkClient.GetPostponedInformation(GroupInfo.VkId);
+            //VkResponse response = _vkClient.GetPostponedInformation(GroupInfo.VkId);
+            VkResponse response = _vkClient.ApiMethodGet($"execute.postponedInf?gid=-{GroupInfo.VkId}");
 
             if (response.isCorrect)
                 if (Convert.ToString(response.tokens[1]) != "")
@@ -77,7 +78,8 @@ namespace groupbot.Models
             {
                 photoParams = Convert.ToString(photo).Split('_');
                 //копируем фото в альбом бота
-                response = _vkClient.CopyPhoto(photoParams[0], photoParams[1], photoParams[2]);
+                //response = _vkClient.CopyPhoto(photoParams[0], photoParams[1], photoParams[2]);
+                response = _vkClient.ApiMethodGet($"execute.CopyPhoto?owner_id={photoParams[0]}&photo_id={photoParams[1]}&access_key={photoParams[2]}");
 
                 //записываем адресса сохраненных пикч
                 if (response.isCorrect)
@@ -265,9 +267,10 @@ namespace groupbot.Models
 
         public int[] Alignment(bool getInf)
         {
-            VkResponse response = _vkClient.SearchDelayInPosts(GroupInfo.VkId, GroupInfo.Offset);
+            //VkResponse response = _vkClient.SearchDelayInPosts(GroupInfo.VkId, GroupInfo.Offset);
+            VkResponse response = _vkClient.ApiMethodGet($"execute.delaySearch?gid=-{GroupInfo.VkId}&offset={GroupInfo.Offset}");
             JToken jo = response.tokens;
-            string text = "";
+            string text ;
 
             if (response.isCorrect)
             {
